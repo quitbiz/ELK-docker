e513870f3e54f5c10f0b54e06f7f6e6b52ce3d18
Just P <>
2023-03-21 07:55:21 +0000

    Initial commit

---
+++ replit.nix
@@ -0,0 +1,12 @@
+{ pkgs }: {
+    deps = [
+        pkgs.sudo
+        pkgs.docker-compose
+        pkgs.docker-compose
+        pkgs.docker-compose
+        pkgs.docker-compose
+        pkgs.docker-compose
+        pkgs.bashInteractive
+        pkgs.man
+    ];
+}
